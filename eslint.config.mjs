b1aee098807bc9f802e3132b78e68b4b3189ebcf
louai <boumedienelouai2@gmail.com>
2025-01-29 20:30:57 +0000

    build: enhace eslint + prettier configs + reformat project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,23 +10,28 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:tailwindcss/recommended",
-  ),
+  {
+    ignores: [
+      "**/*.css",
+      "**/*.scss",
+      "**/*.md",
+      "**/*.yaml",
+      "**/*.yml",
+      "**/*.json",
+      "!**/.eslintrc.json",
+      "!**/.prettierrc.json",
+      "**/*Dockerfile*",
+      "**/*.txt",
+      "**/*.sh",
+    ],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
+      "prefer-template": ["error"],
       semi: "error",
       "prefer-const": "error",
-      "tailwindcss/classnames-order": "error",
     },
-    overrides: [
-      {
-        files: ["*.ts", "*.tsx", "*.js"],
-        parser: "@typescript-eslint/parser",
-      },
-    ],
   },
 ];
 
