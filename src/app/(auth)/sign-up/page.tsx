0fc024779773079e443407f5dd0476a07b386e48
louai <boumedienelouai2@gmail.com>
2025-02-07 20:33:34 +0000

    feat: implement sign up form

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,31 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { SignupForm } from "@/features/auth/componenets/signup-form";
+import { cn } from "@/lib/utils";
+
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+    <div className={cn("flex flex-col gap-6")}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Welcome</CardTitle>
+          <CardDescription>Sign Up to Skill Craft</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignupForm />
+        </CardContent>
+      </Card>
+      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
--- src/features/auth/componenets/signup-form.tsx
+++ src/features/auth/componenets/signup-form.tsx
@@ -1,3 +1,106 @@
+"use client";
+
+import { z } from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { signupSchema } from "../schemas";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { PasswordInput } from "@/components/ui/password-input";
+
 export const SignupForm = () => {
-  return <div>SignupForm</div>;
+  const form = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof signupSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <div className="grid gap-6">
+        <Button variant="outline" className="w-full">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path
+              d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+              fill="currentColor"
+            />
+          </svg>
+          Login with Google
+        </Button>
+
+        <div className="flex gap-x-2 items-center justify-center">
+          <span className="flex-1 h-1 bg-secondary" />
+          <p className="whitespace-nowrap px-2">Or continue with</p>
+          <span className="flex-1 h-1 bg-secondary" />
+        </div>
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-6">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Type your email"
+                    {...field}
+                    type="email"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex items-center">
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <a
+                    href="#"
+                    className="ml-auto text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <FormControl>
+                  <PasswordInput {...field} placeholder="Type your password" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full">
+            Sign Up
+          </Button>
+        </form>
+        <div className="text-center text-sm">
+          Already have an account?{" "}
+          <a href="/sign-in" className="underline underline-offset-4">
+            Sign in
+          </a>
+        </div>
+      </div>
+    </Form>
+  );
 };
