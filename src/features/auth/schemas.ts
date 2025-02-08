29268d7cbd0b0eb6784ce6883cece227ddc49a1e
louai <boumedienelouai2@gmail.com>
2025-02-08 20:12:14 +0000

    feat: setup register route

--- src/features/auth/schemas.ts
+++ src/features/auth/schemas.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const signupSchema = z.object({
+export const authSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8, "Minimum of 8 characters required"),
 });
