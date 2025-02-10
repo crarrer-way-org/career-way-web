81b04cf5b9c5d2eaf8e264d522c160a44ef7e6e0
louai <boumedienelouai2@gmail.com>
2025-02-10 20:14:56 +0000

    chore: some small changes

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,10 +1,4 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { AuthForm } from "@/features/auth/componenets/auth-form";
 import { cn } from "@/lib/utils";
 
@@ -13,8 +7,7 @@ const SignupPage = () => {
     <div className={cn("flex flex-col gap-6")}>
       <Card>
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">Welcome</CardTitle>
-          <CardDescription>Sign Up to Skill Craft</CardDescription>
+          <CardTitle className="text-xl">Access your account</CardTitle>
         </CardHeader>
         <CardContent>
           <AuthForm variant="sign-up" />
